7db63d12ede0483acf1f4be149ad32a3e701621d
LogotipX <Starikov1000@gmail.com>
2023-02-06 15:17:31 +0500

    refactor: border to global styles

--- src/components/HeaderPanel/HeaderPanel.tsx
+++ src/components/HeaderPanel/HeaderPanel.tsx
@@ -7,7 +7,7 @@ import ArrowBack from '../../assets/icons/ArrowBack.svg';
 
 export function HeaderPanel() {
     return (
-        <div className="header-panel bg-main text-simple">
+        <div className="header-panel bg-main text-simple border-main-b">
             <div className="btns-container pl-6 py-3_5">
                 <img src={MenuSquares} />
                 <img src={ArrowBack} />
