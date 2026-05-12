05db9cba2af4804674cde14a13b7131d20607720
LogotipX <Starikov1000@gmail.com>
2026-05-12 21:16:16 +0500

    feat: style navbar

--- src/components/SideNavbar/SideNavbar.tsx
+++ src/components/SideNavbar/SideNavbar.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import './SideNavbar.style.scss';
 
 export function SideNavbar() {
-    return <div>SideNavbar</div>;
+    return (
+        <div className="side-navbar bg-main text-main border-main-r border-main-b">
+            asd
+        </div>
+    );
 }
